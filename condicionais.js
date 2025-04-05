866ae3d1c053d07e13c70ec75be93a17e9a2762b
Diego <diegocaue7@gmail.com>
2025-04-04 21:51:27 -0300

    Exercicios Basico JS

---
+++ condicionais.js
@@ -0,0 +1,99 @@
+function exercicio1 () {
+
+
+let numero = prompt ("Qual sua idade")
+
+if (numero >18 ) { 
+    console.log ("maior de idade")
+}
+
+else {
+    if (numero <18);
+console.log ("menor de idade")
+}
+}
+
+function exercicio02 () {
+
+let numero = prompt("Digite um numero")
+
+if (numero >0) {
+
+    console.log ("Positivo");
+} 
+
+else {
+    if (numero <0 )  
+    console.log("negativo");
+}
+}
+
+function exercicio03 () {
+let nota = prompt ("Nota do aluno")
+
+if (nota >=60)
+    alert ("Aprovado")
+
+else {
+    alert ("Reprovado")
+}
+}
+
+function exercicio04 () {
+let numero = prompt ("Digite um numero")
+ 
+if (numero >0)
+    alert ("Positivo")
+
+if (numero >=0)
+    alert ("igual a Zero")
+
+else 
+    alert("negativo")
+}
+
+function exercicio05 () {
+let numero = prompt ("Classificacao de idade")
+
+if (numero <=12)
+    alert ("criança")
+
+if (numero <=17)
+if (numero >=13)
+    alert ("adolescente")
+
+if (numero >=18)
+    alert ("adulto")
+}
+
+function exercicio06 () {
+let numero = prompt ("Digite um numero")
+
+if (numero %2==0)
+    alert ("numero Par")
+
+else 
+    alert ("Numero Impar")
+}
+
+function exercicioIntermediario1 () {
+    let numero = 1
+    let numero = 2
+    operacao = 1+2
+}
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-alert("Hello Word na tela!");
+// alert("Hello Word na tela!");
 
-console.log ("Hello WOrd no console!");
+// console.log ("Hello WOrd no console!");
+
+let nome = "Diego";
+const idade = 21;
+var cidade = "Sao Paulo";
+
+console.log(nome)
+console.log(idade)
+console.log(cidade)
+
+console.log("Meu nome e: " + nome)  */concatenacao*/
+console.log(`Meu nome e: ${nome}`)  */interpolacao*/
+
+console.log(`Tenho ${idade} anos`)
+console.log(`Minha cidade e: ${cidade}`)
+
+console.log(typeof idade)
+
+let n1 = 10;
+let n2 = 10;
+
+let soma = n1 + n2;
+
+console.log(soma)
+
+
+
